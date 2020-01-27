b03c9edf4e0c3ba8151ef9f6f691471c998ab8ec
Jake Whiteley <jake@craftedbygc.com>
2020-01-27 20:54:36 +0000

    remove dist folder

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,13 +3,4 @@ const mix = require('laravel-mix')
 mix.setPublicPath('/')
 
 
-if (mix.inProduction() === false) {
-    mix.webpackConfig({
-          devtool: 'source-map'
-      })
-      .sourceMaps()
-      .js('src/e.js', 'dist/e.js')
-      .js('examples/test.js', 'examples/test.min.js')
-} else {
-    mix.js('src/e.js', 'dist/e.min.js')
-}
+mix.js('examples/test.js', 'examples/test.min.js')
